f832a24c48d99ad807987859589d00ce30fb7b5e
Loofinka <142446900+Loofinka@users.noreply.github.com>
2023-08-26 14:50:11 +0300

    1.0.0.0
    -- -- -- -- -- -- RELEASE DATE -- -- -- -- -- -- --
    
    -Added new method : Download (string link, string path, string name)
    {
        This method allows you to download any file!
    }
    -Added new method : UpdateCheck (string link, string curVersion) 
    {
        This method allows you to check the update of your program!
    }

---
+++ Internet.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Windows.Forms;
+
+namespace AULib
+{
+    internal class Internet
+    {
+        public static bool IsAvailable()
+        {
+            try
+            {
+                Dns.GetHostEntry("microsoft.com");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static void isNotAvailable()
+        {
+            MessageBox.Show("Please check your internet connection!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Windows.Forms;
+
+namespace AULib
+{
+    public class Main
+    {
+        public static string lastUpdateLink;
+        public static string applicationFolder;
+        public static string applicationName;
+
+        public static void UpdateCheck(string link, string version)
+        {
+            using(WebClient client = new WebClient())
+            {
+                Link1:
+                if(Internet.IsAvailable())
+                {
+                    string readversion = client.DownloadString(link);
+
+                    if (version == readversion)
+                    {
+                        
+                    }
+                    else
+                    {
+                        DialogResult result = MessageBox.Show("Your version outdate! Current Version : " + version + " | New Version : " + readversion, "Error!", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+                        if(result == DialogResult.Retry)
+                        {
+                            goto Link1;
+                        }
+                        else
+                        {
+                            if(result == DialogResult.Ignore)
+                            {
+                                DownloadLastUpdate();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Internet.isNotAvailable();
+                }
+            }
+        }
+        private static void DownloadLastUpdate()
+        {
+            Download(lastUpdateLink, applicationFolder, applicationName);
+        }
+
+        public static void Download(string link, string path, string name)
+        {
+            using(WebClient client = new WebClient()) 
+            {
+                if (Internet.IsAvailable())
+                {
+                    client.DownloadFileAsync(new Uri(link), path + name);
+                }
+                else
+                {
+                    Internet.isNotAvailable();
+                }
+            }
+        }
+    }
+}
